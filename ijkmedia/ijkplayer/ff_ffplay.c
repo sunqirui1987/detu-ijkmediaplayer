8a48a29385eac5bca6e63677ac44e7e5ab30fa82
chao <wangc@detu.com>
2017-03-06 17:50:30 +0800

    fix:Android设置tcp无效的问题

--- ijkmedia/ijkplayer/ff_ffplay.c
+++ ijkmedia/ijkplayer/ff_ffplay.c
@@ -2523,8 +2523,13 @@ static int read_thread(void *arg)
         av_log(ffp, AV_LOG_WARNING, "remove 'timeout' option for rtmp.\n");
         //av_dict_set(&ffp->format_opts, "timeout", NULL, 0);
     }
-    
-   
+    if(av_stristart(is->filename, "rtsp", NULL)) {
+        //这里为了解决android设置rtsp设置tcp连接无效的问题
+        AVDictionaryEntry * entry = av_dict_get(ffp->format_opts, "rtsp_transport", NULL, AV_DICT_MATCH_CASE);
+        if(entry != NULL) {
+            av_dict_set(&ffp->format_opts, entry->key, entry->value, 0);
+        }
+    }
     
     if (ffp->iformat_name)
         is->iformat = av_find_input_format(ffp->iformat_name);
