cfe0a8d439c7fe3f2c54fbf31cd4b8cdec2e93a5
chenliang <chenliang@detu.com>
2017-09-28 18:18:47 +0800

    modify: 解码失败错误码-22时，上报-400错误码。

--- ijkmedia/ijkplayer/ff_ffplay.c
+++ ijkmedia/ijkplayer/ff_ffplay.c
@@ -504,7 +504,7 @@ static int decoder_decode_frame(FFPlayer *ffp, Decoder *d, AVFrame *frame, AVSub
 		//fprintf(fp_log, "%s, frame->pts:%ld, pkt_temp.pts:%ld\n", d->avctx->codec_type == AVMEDIA_TYPE_VIDEO ? "video" : "audio", frame->pts, d->pkt_temp.pts);
         if (ret < 0) {
             d->packet_pending = 0;
-			if (ret == -40){
+			if (ret == -40 || ret == -22){
 				ffp_notify_msg2(ffp, FFP_MSG_ERROR, FFP_MSG_ERROR_CODE_CODEC);
 				av_log(ffp, AV_LOG_DEBUG, "Not support, stop decode in second.");
 				return -1;
